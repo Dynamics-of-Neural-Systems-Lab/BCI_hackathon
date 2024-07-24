ca9b80b78df879d9615b4e37a330213f9ee75ea9
Moritz Schaefer <mail@moritzs.de>
2024-07-24 19:32:02 +0200

    Add VRNN with basic unsupervised training file

---
+++ train_vrnn.py
@@ -0,0 +1,185 @@
+"""
+Training with VRNN
+"""
+
+import torch
+import torch.nn as nn
+import torch.utils
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+from VariationalRecurrentNeuralNetwork.model import VRNN
+from utils.train import TrainConfig
+
+from tqdm import tqdm
+
+from utils.augmentations import get_default_transform
+from utils import creating_dataset
+
+
+DATA_PATH = "/home/moritz/Projects/BCI_hackathon/data/dataset_v2_blocks"
+train_config = TrainConfig(
+    exp_name="test_2_run_fedya",
+    p_augs=0.3,
+    batch_size=64,
+    eval_interval=150,
+    num_workers=8,
+)
+
+
+def get_dataset(train_config):
+    ## Data preparation
+    transform = get_default_transform(train_config.p_augs)
+    data_paths = dict(
+        datasets=[DATA_PATH],
+        hand_type=["left", "right"],  # [left, 'right']
+        human_type=["health", "amputant"],  # [amputant, 'health']
+        test_dataset_list=["fedya_tropin_standart_elbow_left"],
+    )
+    data_config = creating_dataset.DataConfig(**data_paths)
+    train_dataset, test_dataset = creating_dataset.get_datasets(
+        data_config, transform=transform
+    )
+    return train_dataset, test_dataset
+
+
+def train(train_loader, epoch):
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(tqdm(train_loader)):
+
+        # transforming data
+        data = data.to(device)
+
+        # restructure `data` such that always 8 consecutive time steps are given to the model at once
+        data = data.view(data.shape[0], 8, 32, 8)  # Shape: [B, C, T/8, 8]
+        data = data.permute(0, 1, 3, 2)  # Shape: [B, C, 8, T/8]
+        data = data.contiguous().view(data.shape[0], 8 * 8, 32)  # Shape: [B, C*8, T/8]
+
+        data = data.permute(2, 0, 1)  # [B, C, T] -> [T, B, C]
+        data = (data - data.min()) / (data.max() - data.min())
+
+        # forward + backward + optimize
+        optimizer.zero_grad()
+        kld_loss, nll_loss, _, _ = model(data)
+        loss = kld_loss + nll_loss
+        loss.backward()
+        optimizer.step()
+
+        # grad norm clipping, only in pytorch version >= 1.10
+        nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        # printing
+        if batch_idx % print_every == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\t KLD Loss: {:.6f} \t NLL Loss: {:.6f}".format(
+                    epoch,
+                    batch_idx * batch_size,
+                    batch_size * (len(train_loader.dataset) // batch_size),
+                    100.0 * batch_idx / len(train_loader),
+                    kld_loss / batch_size,
+                    nll_loss / batch_size,
+                )
+            )
+
+            sample = model.sample(32)
+            plt.imshow(sample.to(torch.device("cpu")).numpy())
+            plt.pause(1e-6)
+
+        train_loss += loss.item()
+
+    print(
+        "====> Epoch: {} Average loss: {:.4f}".format(
+            epoch, train_loss / len(train_loader.dataset)
+        )
+    )
+
+
+def test(test_loader, epoch):
+    """uses test data to evaluate
+    likelihood of the model"""
+
+    mean_kld_loss, mean_nll_loss = 0, 0
+    with torch.no_grad():
+        for i, (data, _) in enumerate(test_loader):
+
+            data = data.to(device)
+            # restructure `data` such that always 8 consecutive time steps are given to the model at once
+            data = data.view(data.shape[0], 8, 32, 8)  # Shape: [B, C, T/8, 8]
+            data = data.permute(0, 1, 3, 2)  # Shape: [B, C, 8, T/8]
+            data = data.contiguous().view(
+                data.shape[0], 8 * 8, 32
+            )  # Shape: [B, C*8, T/8]
+            data = data.permute(2, 0, 1)  # [B, T, C] -> [T, B, C]
+            data = (data - data.min()) / (data.max() - data.min())
+
+            kld_loss, nll_loss, _, _ = model(data)
+            mean_kld_loss += kld_loss.item()
+            mean_nll_loss += nll_loss.item()
+
+    mean_kld_loss /= len(test_loader.dataset)
+    mean_nll_loss /= len(test_loader.dataset)
+
+    print(
+        "====> Test set loss: KLD Loss = {:.4f}, NLL Loss = {:.4f} ".format(
+            mean_kld_loss, mean_nll_loss
+        )
+    )
+
+
+# changing device
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    torch.cuda.empty_cache()
+else:
+    device = torch.device("cpu")
+
+# hyperparameters
+h_dim = 100
+z_dim = 16
+n_layers = 1
+n_epochs = 25
+clip = 10
+learning_rate = 1e-3
+batch_size = 128  # 128
+seed = 128
+print_every = 1000  # batches
+save_every = 10  # epochs
+
+# manual seed
+torch.manual_seed(seed)
+plt.ion()
+
+train_dataset, test_dataset = get_dataset(train_config)
+
+# init model + optimizer + datasets
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=train_config.batch_size,
+    shuffle=True,
+    num_workers=train_config.num_workers,
+)
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=train_config.batch_size,
+    shuffle=True,
+    num_workers=train_config.num_workers,
+)
+
+model = VRNN(
+    x_dim=8 * 8,  # chunking 8 time steps so this becomes 64
+    h_dim=32,  # hidden recurrent state
+    z_dim=16,  # latent space (could become 16)
+    n_layers=1,
+)  # how many recurrent layers
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(1, n_epochs + 1):
+    # training + testing
+    train(train_loader, epoch)
+    test(test_loader, epoch)
+
+    # saving model
+    # if epoch % save_every == 1:
+    #     fn = "saves/vrnn_state_dict_" + str(epoch) + ".pth"
+    #     torch.save(model.state_dict(), fn)
+    #     print("Saved model to " + fn)
